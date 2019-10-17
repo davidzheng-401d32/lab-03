e5605397233e0c6d10b1949959cf753c9e25dcb3
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2019-10-16 17:35:36 -0700

    wrote tests for call back

--- __tests__/file-reader.test.js
+++ __tests__/file-reader.test.js
@@ -2,15 +2,26 @@
 
 jest.mock('fs');
 
-const fileReader = require('../lib/file-reader');
+const fileReaderCallBack = require('../lib/file-reader-callback.js');
+
 
 describe('Testing file reader module', () => {
+//----------------------------bad path for call back------------------------------------//
   it('Throws err when bad file path is given', (done) => {
     let file = `${__dirname}../data/bad.txt`;
-    fileReader(file, (err, data) => {
+    fileReaderCallBack(file, (err, data) => {
       expect(err).toBeDefined();
       expect(data).not.toBeDefined();
       expect(err).toEqual('Invalid File');
+      done();
+    });
+  });
+  //---------------------------good path for call back--------------------------------------//
+  it('no err when good path is given', (done) => {
+    let file = `${__dirname}../data/person.json`;
+    fileReaderCallBack(file, (err, data) => {
+      expect(err).toBe(null);
+      expect(data).toBeDefined();
       done();
     });
   });
